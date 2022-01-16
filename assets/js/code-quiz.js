244ca520e11c1e10811b419d2961dd44cfcf95f3
Diego Kaku <diegokaku@gmail.com>
2022-01-15 19:00:07 -0600

    correct wrong msg

--- assets/js/code-quiz.js
+++ assets/js/code-quiz.js
@@ -15,12 +15,18 @@ let questionEl = document.createElement("div");
 // the question
 let questionQuestion = document.createElement("h2");
 // unordered list for the options
-let questionList = document.createElement("ul");
+let questionList = document.createElement("ol");
 // each options
 let question1 = document.createElement("li");
 let question2 = document.createElement("li");
 let question3 = document.createElement("li");
 let question4 = document.createElement("li");
+
+// correct under the question
+let correctMsg = document.createElement("p");
+correctMsg.className = "correct-msg";
+correctMsg.setAttribute("id", "correctmsg");
+correctMsg.textContent = "Correct";
 
 const questionBank = [
   {
@@ -164,13 +170,15 @@ let startGameBtn = (startBtn.onclick = function () {
   body.appendChild(questionEl);
   // add question h2 to the wrap div
   questionEl.appendChild(questionQuestion);
-  //add ul to the wrap div
+  //add ol to the wrap div
   questionEl.appendChild(questionList);
-  // add li to the ul
+  // add li to the ol
   questionList.appendChild(question1);
   questionList.appendChild(question2);
   questionList.appendChild(question3);
   questionList.appendChild(question4);
+  // correct wrong msg
+  body.appendChild(correctWrong);
 });
 
 // let removeWelcome = (startBtn.onclick = function () {
@@ -181,3 +189,47 @@ let startGameBtn = (startBtn.onclick = function () {
 // };
 
 // possible way to verify answers
+
+// (function(){
+//   function byId(id) {
+//     return document.getElementById(id);
+//   }
+//   byId('submitter').onclick = function() {
+
+//     var wrongAnswers = [ ];
+
+//     if (!byId('answer-1-1').checked) { wrongAnswers.push(1); }
+//     if (!byId('answer-2-2').checked) { wrongAnswers.push(2); }
+//     if (!byId('answer-3-3').checked) { wrongAnswers.push(3); }
+//     if (!byId('answer-4-2').checked) { wrongAnswers.push(4); }
+//     if (!byId('answer-5-2').checked) { wrongAnswers.push(5); }
+//     if (!byId('answer-6-4').checked) { wrongAnswers.push(6); }
+//     if (!byId('answer-7-4').checked) { wrongAnswers.push(7); }
+//     if (!byId('answer-8-5').checked) { wrongAnswers.push(8); }
+//     if (!byId('answer-9-1').checked) { wrongAnswers.push(9); }
+//     if (!byId('answer-10-2').checked) { wrongAnswers.push(10); }
+
+//     var message;
+
+//     if (wrongAnswers.length === 0) {
+//       message = 'Flawless victory.';
+//     }
+//     else if (wrongAnswers.length <= 8) {
+//       message = 'Very good, but not quite there yet.';
+//     }
+//     else if (wrongAnswers.length < 6) {
+//       message = 'That\'s more than a half :(';
+//     }
+//     else {
+//       message = 'Ouch.';
+//     }
+
+//     var prefix = (wrongAnswers.length === 10) ? 'all ' : '';
+
+//     document.getElementById('quiz-result').innerHTML = (
+//       'You\'ve got ' + prefix + '<strong>' + wrongAnswers.length +
+//       '</strong> answer'+ (wrongAnswers.length === 1 ? '' : 's') +
+//       ' wrong' + ((wrongAnswers.length > 0) ? (' (' + '#' + wrongAnswers.join(', #') + ')') : '')
+//       + '.<br>' + message);
+//   };
+// })();
